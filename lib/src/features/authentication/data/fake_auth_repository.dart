b3c00cfbd8dc2933e45f37d3dbb0d3354e368390
Michael Barilla <michaelcbarilla@gmail.com>
2025-05-27 15:52:19 -0400

    add current user tests for fake auth repo - use delay util for fake auth repo - add equality and to string methods for app user - add test cases for current user

--- lib/src/features/authentication/data/fake_auth_repository.dart
+++ lib/src/features/authentication/data/fake_auth_repository.dart
@@ -1,15 +1,23 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:lifted/src/features/authentication/domain/app_user.dart';
+import 'package:lifted/src/utils/delay.dart';
 import 'package:lifted/src/utils/in_memory_store.dart';
 
 class FakeAuthRepository {
+  FakeAuthRepository({
+    this.addDelay = true,
+  });
+
+  final bool addDelay;
   final _authState = InMemoryStore<AppUser?>(null);
 
   Stream<AppUser?> authStateChanges() => _authState.stream;
   AppUser? get currentUser => _authState.value;
 
   Future<void> signInWithEmailAndPassword(String email, String password) async {
-    await Future.delayed(const Duration(seconds: 1));
+    await delay(addDelay);
     if (currentUser == null) {
       _createNewUser(email);
     }
@@ -17,14 +25,14 @@ class FakeAuthRepository {
 
   Future<void> createUserWithEmailAndPassword(
       String email, String password) async {
-    await Future.delayed(const Duration(seconds: 1));
+    await delay(addDelay);
     if (currentUser == null) {
       _createNewUser(email);
     }
   }
 
   Future<void> signOut() async {
-    await Future.delayed(const Duration(seconds: 1));
+    await delay(addDelay);
     // throw Exception('Connection failed');
     _authState.value = null;
   }
