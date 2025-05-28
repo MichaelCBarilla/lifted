329c477ea8bf5751903f9799cdbd503608a5fdb2
Michael Barilla <michaelcbarilla@gmail.com>
2025-05-28 08:57:38 -0400

    create auth robot and account screen initial test - use robot testing to make widget testing more human readable

---
+++ test/src/features/authentication/presentation/account/account_screen_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../auth_robot.dart';
+
+void main() {
+  testWidgets('Cancel Logout', (tester) async {
+    final r = AuthRobot(tester);
+    await r.pumpAccountScreen();
+    await r.tapLogoutButton();
+    r.expectLogoutDialogFound();
+    await r.tapCancelButton();
+    r.expectLogoutDialogNotFound();
+  });
+}
